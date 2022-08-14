d0b309e90dc77dd25759c11348bb07283fdec909
robertohealy <healybobby@gmail.com>
2022-08-14 03:46:19 -0500

    Adding organization

--- src/components/UserCard.jsx
+++ src/components/UserCard.jsx
@@ -1,7 +1,7 @@
 import { Button } from "./Button";
 import Image from "next/image";
 
-const example = {"first": "Bobby", "last": "Healy"};
+const example = {"first": "Bobby", "last": "Healy", "organization" : "Freelance"};
 
 export function UserCard({user = example, openModal}) {
     return (
@@ -15,7 +15,7 @@ export function UserCard({user = example, openModal}) {
                         </div>
                     </div>
                     
-                    <div className="w-full text-center mt-20">
+                    {/* <div className="w-full text-center mt-20">
                         <div className="flex justify-center lg:pt-4 pt-8 pb-0">
                             <div className="p-3 text-center">
                                 <span className="text-xl font-bold block uppercase tracking-wide text-slate-700">3,360</span>
@@ -31,12 +31,12 @@ export function UserCard({user = example, openModal}) {
                                 <span className="text-sm text-slate-400">Following</span>
                             </div>
                         </div>
-                    </div>
+                    </div> */}
                 </div>
                 <div className="text-center mt-2">
                     <h3 className="text-2xl text-slate-700 font-bold leading-normal mb-1">{user.first} {user.last}</h3>
                     <div className="text-xs mt-0 mb-2 text-slate-400 font-bold uppercase">
-                        <i className="fas fa-map-marker-alt mr-2 text-slate-400 opacity-75"></i>Paris, France
+                        <i className="fas fa-map-marker-alt mr-2 text-slate-400 opacity-75"></i>{user.organization}
                     </div>
                 </div>
                 <div className="mt-6 py-6 border-t border-slate-200 text-center">
--- src/pages/create.jsx
+++ src/pages/create.jsx
@@ -105,6 +105,20 @@ function Create() {
                                 />
                             </div>
 
+                            <div className="col-span-6 sm:col-span-4">
+                                <label htmlFor="organization" className="block text-sm font-medium text-gray-700">
+                                    Organization
+                                </label>
+                                <input
+                                type="text"
+                                name="organization"
+                                id="organization"
+                                className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
+                                value={newCard.organization}
+                                onChange={handleChange}
+                                />
+                            </div>
+
                             </div>
                         </div>
                         </div>
